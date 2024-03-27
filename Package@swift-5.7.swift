2106f3db32fc54c0345f65022d42c4a5aa9d8e07
Liam <liam_i@163.com>
2024-03-27 18:31:28 +0800

    fix: swift-tools-version: 5.7.1

--- Package@swift-5.7.swift
+++ Package@swift-5.7.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "AutoFlex",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v11),
         .macOS(.v11),
-        .tvOS(.v12)
+        .tvOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
